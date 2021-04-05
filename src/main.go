21d1d6f6f4e3b7b82fe1b734458ff1a0f0bc310c
morepotentials <34422494+morepotentials@users.noreply.github.com>
2021-04-05 12:47:48 +0200

    Basic SetUp GoAPI VueApp

--- src/main.go
+++ src/main.go
@@ -7,42 +7,66 @@ import (
 )
 
 func main() {
-		http.HandleFunc("/users", ListUsers)
-		http.ListenAndServe(":8080", nil)
+	paulo := User{
+		ID: 0,
+		FirstName: "Paulo",
+		LastName: "Engelke",
+		Email:"peengelke(at)gmail.com",
+	}
+	erik := User{
+		ID: 1,
+		FirstName: "Erik",
+		LastName: "Haight",
+		Email:"ehaight(at)sojern.com",
+	}
+	users := make([]User, 0)
+	users = append(users, paulo) 
+	users = append(users, erik)
+
+	MyService:= Service{
+		Host: ":8080",
+		Users: users,
+	}
+	http.HandleFunc("/users", MyService.ListUsers)
+	MyService.start()
+}
+
+type UsersFetcher interface {
+	ListUsers() ([]User, error)
+}
+type Service struct {
+	Host string
+	Users []User
+	Fetcher UsersFetcher 
+}
+
+func (S Service) start(){
+	http.ListenAndServe(S.Host, nil)
 }
 
 type User struct {
 	ID int `json:"id"`
 	FirstName string `json:"first_name"`
 	LastName string `json:"last_name"`
+	Email string `json:"email"`
 }
 
-func ListUsers(w http.ResponseWriter, r *http.Request){
-	paulo := User{
-		ID: 0,
-		FirstName: "Paulo",
-		LastName: "Engelke",
-	}
-	erik := User{
-		ID: 1,
-		FirstName: "Erik",
-		LastName: "Haight",
-	}
-	users := make([]User, 0)
-	users = append(users, paulo) 
-	users = append(users, erik)
-	fmt.Println(users)
+func (S Service) ListUsers(w http.ResponseWriter, r *http.Request){
+	fmt.Println(S.Host)
+	fmt.Println(S.Users)
 
-	bytes, err := json.Marshal(users)
+	bytes, err := json.Marshal(S.Users)
 	if err != nil {
 		fmt.Println(err)
 		return 
 	}
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	_, err = w.Write(bytes)
 	if err !=nil{
 		fmt.Println(err)
 		return
 	}
-
+	fmt.Println("hello")
 
 }
--- vue/app.js
+++ vue/app.js
@@ -1,34 +1,56 @@
-import axios from 'axios'
-
-const app = Vue.createApp({
-  data(){
-    return {
-      aSel: false,
-      bSel: false,
-      cSel: false,
-      arr_users: []
-    };
-  },
-  mounted() {
-    axios({ method: "GET", "url": "localhost:8080/users" }).then(result => {
-        this.arr_users = result;
-        console.log(result);
-    }, error => {
-        console.error(error);
-    });
-  },
-  methods: {
-    boxSel(box) {
-      if (box === 'A'){
-        this.aSel = true;
-      } else if (box === 'B'){
-        this.bSel = true;
-      } else if (box === 'C'){
-        this.cSel = true;
-
-      }
+Vue.component(
+  "user-component",
+  {
+  props: [
+    'user'
+  ],
+  filters: {
+    fullName(user){
+      return `${user.first_name} ${user.last_name}`
     }
-  }
-});
+  },
+  template: `
+  <div>
+    <h4>{{user.id}}</h4>
+    <h5>First name is {{user | fullName}}</h5>
+  </div>
+  `
+  } 
+);
 
-app.mount("#styling");
+
+const app = new Vue({
+  el:"#app",
+  data: {
+      arr_users: [
+        {
+          id:2, 
+          first_name: "Mason", 
+          last_name: "child"
+        }, 
+        {
+          id:3, 
+          first_name: "Ben", 
+          last_name: "Kingsley"
+        }
+      ],
+  },
+  mounted(){
+    fetch("http://localhost:8080/users")
+    .then(response=>response.json())
+    .then((data)=>{
+      this.arr_users=data
+    })
+  },
+  template: `
+  <div>
+    <header>
+      <h1>Vue Dynamic Styling</h1>
+    </header>
+    <section id="styling">
+      <user-component v-for="user in arr_users" v-bind:user="user"/>
+
+    </section>
+  </div>`
+})
+
