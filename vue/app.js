fe6426f1000c273f29eb998dc5c1771aca2fa454
morepotentials <34422494+morepotentials@users.noreply.github.com>
2021-04-17 19:08:30 +0200

    go api first commit

--- vue/app.js
+++ vue/app.js
@@ -9,7 +9,8 @@ Vue.component(
       return `${user.first_name} ${user.last_name}`
     }
   },
-  template: `
+  template: 
+  `
   <div>
     <h4>{{user.id}}</h4>
     <h5>First name is {{user | fullName}}</h5>
@@ -22,27 +23,15 @@ Vue.component(
 const app = new Vue({
   el:"#app",
   data: {
-      arr_users: [
-        {
-          id:2, 
-          first_name: "Mason", 
-          last_name: "child"
-        }, 
-        {
-          id:3, 
-          first_name: "Ben", 
-          last_name: "Kingsley"
-        }
-      ],
+      arr_users: [],
   },
   mounted(){
     fetch("http://localhost:8080/users")
     .then(response=>response.json())
-    .then((data)=>{
-      this.arr_users=data
-    })
+    .then(data=>this.arr_users=data)
   },
-  template: `
+  template: 
+  `
   <div>
     <header>
       <h1>Vue Dynamic Styling</h1>
@@ -51,6 +40,7 @@ const app = new Vue({
       <user-component v-for="user in arr_users" v-bind:user="user"/>
 
     </section>
-  </div>`
+  </div>
+  `
 })
 
